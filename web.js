6888713244537fcd69e2328ee14f9f953d91ac2c
NavSha <navtheunstoppable@gmail.com>
2013-07-20 09:55:29 +0000

    web.js

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var fs = require('fs');
-var buf = new Buffer(100);
+var buf = new Buffer(fs.readFileSync("index.html"));
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send(buf.toString("utf-8", fs.readFileSync("index.html")));
+   //buf = fs.readFileSync("index.html");
+    response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
